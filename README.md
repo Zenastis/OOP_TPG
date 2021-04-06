db783df1f7dfe1bacf204370bea593d9c8ae9303
samuel mcswain <samuelmcswain@hotmail.com>
2021-04-05 21:55:11 -0400

    demo video uploaded

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # OOP_TPG
+
+
+https://drive.google.com/file/d/1RJG9gW91Dts8Z93j7gi6TDrDNOX7dbsk/view
--- index.js
+++ index.js
@@ -3,145 +3,142 @@
 
 
 
-const Manager = require ("./lib/Manager")
-const Intern = require ("./lib/Intern")
-const Engineer = require ("./lib/Engineer")
-const inquirer = require ("inquirer")
-const fs = require ("fs")
-const path = require ("path")
+const Manager = require("./lib/Manager")
+const Intern = require("./lib/Intern")
+const Engineer = require("./lib/Engineer")
+const inquirer = require("inquirer")
+const fs = require("fs")
+const path = require("path")
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 const team = []
 const id = []
 
-function runTeam(){
-function intern (){
+
+function intern() {
     inquirer.prompt([
         {
-           type:"input",
-           name:"internName",
-           message:"what is the interns name" 
+            type: "input",
+            name: "internName",
+            message: "what is the interns name"
         },
         {
-            type:"input",
-            name:"internId",
-            message:"what is the interns id"  
+            type: "input",
+            name: "internId",
+            message: "what is the interns id"
         },
         {
-            type:"input",
-            name:"internEmail",
-            message:"what is the interns email" 
+            type: "input",
+            name: "internEmail",
+            message: "what is the interns email"
         },
         {
-            type:"input",
-            name:"internSchool",
-            message:"what is the interns school" 
+            type: "input",
+            name: "internSchool",
+            message: "what is the interns school"
         }
-    ]).then(answers =>{
-        const intern = new Intern(answers.internName,answers.internId,answers.internEmail,answers.internSchool)
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
         team.push(intern)
         id.push(answers.internId)
-        build ()
+        build()
     })
 }
 
-function build(){
+function build() {
     inquirer.prompt([
         {
-            type:"list",
-            name:"myChoice",
-            message:"which employee would you like to add to the team",
-            choices:["engineer","intern","manager","no more people"]
+            type: "list",
+            name: "myChoice",
+            message: "which employee would you like to add to the team",
+            choices: ["engineer", "intern", "manager", "no more people"]
         }
-    ]).then(user =>{
-        switch (myChoice.user){
+    ]).then(user => {
+        switch (user.myChoice) {
             case "intern":
                 intern();
                 break;
-                case "engineer":
+            case "engineer":
                 engineer();
                 break;
-                case "manager":
+            case "manager":
                 manager();
                 break;
-                default:
-                generate ()
+            default:
+                generate()
 
-                
+
         }
     })
 }
 
-function generate (){
+function generate() {
     if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR)
-      }
-      fs.writeFileSync(outputPath, render(team), "utf-8");
+    }
+    fs.writeFileSync(outputPath, render(team), "utf-8");
 
 }
 
-function engineer (){
+function engineer() {
     inquirer.prompt([
         {
-           type:"input",
-           name:"engineerName",
-           message:"what is the engineers name" 
+            type: "input",
+            name: "engineerName",
+            message: "what is the engineers name"
         },
         {
-            type:"input",
-            name:"engineerId",
-            message:"what is the engineers id"  
+            type: "input",
+            name: "engineerId",
+            message: "what is the engineers id"
         },
         {
-            type:"input",
-            name:"engineerEmail",
-            message:"what is the engineers email" 
+            type: "input",
+            name: "engineerEmail",
+            message: "what is the engineers email"
         },
         {
-            type:"input",
-            name:"engineerGithub",
-            message:"what is the engineers github" 
+            type: "input",
+            name: "engineerGithub",
+            message: "what is the engineers github"
         }
-    ]).then(answers =>{
-        const engineer = new Engineer(answers.engineerName,answers.engineerId,answers.engineerEmail,answers.engineerGithub)
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
         team.push(engineer)
         id.push(answers.engineerId)
-        build ()
+        build()
     })
 }
 
-function manager (){
+function manager() {
     inquirer.prompt([
         {
-            type:"input",
-            name:"managerName",
-            message:"what is the managers name" 
+            type: "input",
+            name: "managerName",
+            message: "what is the managers name"
         },
         {
-            type:"input",
-            name:"managerId",
-            message:"what is the managers id"  
+            type: "input",
+            name: "managerId",
+            message: "what is the managers id"
         },
         {
-            type:"input",
-            name:"managerEmail",
-            message:"what is the managers email" 
+            type: "input",
+            name: "managerEmail",
+            message: "what is the managers email"
         },
         {
-            type:"input",
-            name:"managerOfficeNumber",
-            message:"what is the managers office number" 
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "what is the managers office number"
         }
-    ]).then(answers =>{
-        const manager = new Manager(answers.managerName,answers.managerId,answers.managerEmail,answers.managerOfficeNumber)
+    ]).then(answers => {
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
         team.push(manager)
         id.push(answers.managerId)
-        build ()
+        build()
     })
 }
 
-
-
-}
-runTeam()
+build()
